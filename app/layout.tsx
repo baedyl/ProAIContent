8ab6e671cd12f30ee6d33d937d4b1979309ad052
Emmanuel <emmanuelabona@gmail.com>
2025-11-17 07:33:36 -0500

    Add GlobalLoadingOverlay component and integrate it into the RootLayout. This overlay displays a loading spinner during page transitions, enhancing user experience by providing visual feedback while content is loading.

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from 'next/font/google'
 import { Toaster } from 'react-hot-toast'
 import { Providers } from './providers'
 import Navbar from '@/components/Navbar'
+import GlobalLoadingOverlay from '@/components/GlobalLoadingOverlay'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -23,6 +24,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Providers>
           <Navbar />
+        <GlobalLoadingOverlay />
           <Toaster position="top-right" />
           {children}
         </Providers>
